e3f1e3507b0ff33ceb556afc431739e445b64c95
Bamontan <baptiste.demontangon@gmail.com>
2021-05-28 22:08:08 +0200

    emptied python test script

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import time
-my_message = "1616768708864|23|2|Rafael Coss|105|Saint Louis to Memphis|37.47485808497102|-89.71435546875|84|Normal|0|0|0|51"
-for i in range(3):
-   print(my_message, flush=True)
-   time.sleep(1)
