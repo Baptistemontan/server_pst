c3109fd841842cda60c4c3bc79e5668b908a2e22
Bamontan <baptiste.demontangon@gmail.com>
2021-06-03 14:49:41 +0200

    tried another link

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 
 export function connect() {
-    mongoose.connect("mongodb+srv://Read-PST-DB:rX0eCzG9vS5aFKbf@pst-webapp-server.lk9bx.mongodb.net/test", {useNewUrlParser:true, useUnifiedTopology: true }, err => {
+    mongoose.connect("mongodb+srv://Read-PST-DB:rX0eCzG9vS5aFKbf@pst-webapp-server.lk9bx.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true }, err => {
         if(err) {
             console.error(err)
         } else {
